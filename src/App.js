7997efda9cee55615398468688e5ad621afc9220
Vicente Garcia Ayllon <vicentegarciaayllon@MacBook-Pro-DXGJNM92YM.local>
2023-02-20 17:00:11 +0100

    finish pre web

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import './App.scss';
+import React from 'react';
 import waterWomen from '../src/images/water-women.jpeg';
 import revelacionImg from '../src/images/1_REVELACION.png';
 import resistirImg from '../src/images/2_RESISTIR.png';
 import rendicionImg from '../src/images/3_RENDICION.png';
 import integracionImg from '../src/images/4_INTEGRACION.png';
 import iluminarImg from '../src/images/5_ILUMINAR.png';
-import mariposa from '../src/images/mariposa-left.jpeg';
+/* import mariposa from '../src/images/mariposa-left.jpeg'; */
 
 function App() {
+  React.useEffect(() => {
+    const cursor = document.querySelector('.cursor');
+    const moveCursor = (e) => {
+      cursor.style.display = 'block';
+
+      let left = e.pageX;
+      let top = e.pageY;
+
+      cursor.style.left = left + 'px';
+      cursor.style.top = top + 'px';
+    };
+
+    window.addEventListener('mousemove', moveCursor);
+    /* window.addEventListener('mouseout', () => cursor.style.display = 'none'); */
+
+    document.querySelectorAll('footer a').forEach(el => {
+      el.addEventListener('mouseover', () => {
+        el.nextSibling.classList.remove('hidden');
+      });
+      el.addEventListener('mouseout', () => {
+        el.nextSibling.classList.add('hidden');
+      });
+    });
+  }, []);
+
   return (
     <div className="App">
       {/* <header className="App-header">
@@ -21,13 +47,14 @@ function App() {
       </header> */}
       <section className='soon'>
         <h5>s o o n</h5>
-        <h4>la belleza de la oscuridad</h4>
+        <h4>La belleza de la oscuridad</h4>
       </section>
       <section className='brief'>
         <div>
           <p className='brief-text'>La belleza de la oscuridad es un llamado a conectar con tu guía interior para navegar la dualidad de tu ser. Es una apología de la valentía de las almas sensibles que habitan en este mundo.</p>
           <p className='brief-text'>Su símbolo es el círculo, la crisálida como espacio físico, emocional y espiritual protegido para la alquimia de la sombra.</p>
           <p className='brief-text'>Este oráculo contiene poderosos códigos para transmutar oscuridad en luz, activando profundos niveles de sanación en las diferentes fases y estados. Está dividido en 5 portales de oscuridad y luz: la revelación, el resistir, la rendición, la integración y el iluminar.</p>
+          <div className='cursor'></div>
         </div>
         <img className='water-women' src={waterWomen} alt=''></img>
         <div className='water-women-overlay'></div>
@@ -75,24 +102,24 @@ function App() {
         </div>
       </section>
       <section className='intencion'>
-        <img className='mariposa-left' alt='' src={mariposa}></img>
         <p className='intencion-text'>Que esta creación sea una expresión auténtica de mi ser</p>
         <p className='intencion-text'>que pueda inspirar amor incondicional,</p>
         <p className='intencion-text'>un amor que sostiene el dolor de la transformación,</p>
         <p className='intencion-text'>que invita a la aceptación propia,</p>
         <p className='intencion-text'>que permite conectar con el sentido superior, rendirse y confiar.</p>
+        <p className='jimena'>Jimena González</p>
       </section>
       <footer>
         <div className='footer-div'>
-          <a href='#prueba'>mailchimp</a>
+          <a className='subscribe-icon' href='#prueba'> </a>
           <p className='hidden'>Recibe un mail cuando el oráculo esté disponible</p>
         </div>
         <div className='footer-div'>
-          <a href='https://www.instagram.com/soyjimenagonz_/' target="_blank" rel="noopener noreferrer">instagram</a>
-          <p className='hidden'>Redes sociales</p>
+          <a className='instagram-icon' href='https://www.instagram.com/jimenagonzalez.in/' target="_blank" rel="noopener noreferrer"> </a>
+          <p className='hidden'>Sígueme para estar al tanto de este y otros proyectos</p>
         </div>
         <div className='footer-div'>
-          <a href='mailto:jimenagonzalez.in@gmail.com'>contacto</a>
+          <a className='mail-icon' href='mailto:jimenagonzalez.in@gmail.com'> </a>
           <p className='hidden'>Colaboraciones y más</p>
         </div>
       </footer>
