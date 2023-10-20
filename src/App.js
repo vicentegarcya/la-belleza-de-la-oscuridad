df25db0a60a748455509c5e640807d92d0920525
Vicente Garcia <vicentegarciaayllon@gmail.com>
2023-10-20 12:18:08 +0200

    new design

--- src/App.js
+++ src/App.js
@@ -11,38 +11,68 @@ import card7 from "../src/images/7.jpg";
 function App() {
   return (
     <div className="App">
-      <div className="proximamente">PRÓXIMAMENTE</div>
-      <div className="title">La belleza de la oscuridad</div>
-      <div className="cards">
-        <img className="card" src={card1} alt="first card of the deck"></img>
-        <img className="card" src={card2} alt="second card of the deck"></img>
-        <img className="card" src={card3} alt="third card of the deck"></img>
-        <img className="card" src={card4} alt="fourth card of the deck"></img>
-        <img className="card" src={card5} alt="fifth card of the deck"></img>
-        <img className="card" src={card6} alt="sixth card of the deck"></img>
-        <img className="card" src={card7} alt="seventh card of the deck"></img>
-      </div>
+      <div className="proximamente">próximamente</div>
       <div className="explanation">
-        La belleza de la oscuridad es un oráculo para quienes no creen en los
-        oráculos, sino en conectar con su propia sabiduría interna.
+        <p>
+          Un oráculo para quienes no creen en los oráculos, sino en conectar con
+          su propia sabiduría interna.
+        </p>
+        <p>
+          Para quienes han trascendido la positividad tóxica y se sumergen con
+          valentía en la experiencia completa de la vida.
+        </p>
+      </div>
+      <div className="title_cards">
+        <div className="cards">
+          <img className="card" src={card1} alt="first card of the deck"></img>
+          <img className="card" src={card2} alt="second card of the deck"></img>
+          <img className="card" src={card3} alt="third card of the deck"></img>
+          <img className="card" src={card4} alt="fourth card of the deck"></img>
+          <img className="card" src={card5} alt="fifth card of the deck"></img>
+          <img className="card" src={card6} alt="sixth card of the deck"></img>
+          <img
+            className="card"
+            src={card7}
+            alt="seventh card of the deck"
+          ></img>
+        </div>
+        <div className="title">La belleza<br></br> de la oscuridad</div>
       </div>
       <div className="creadores_desktop">
         <p className="supreme">
-          un proyecto del<br></br>ecosistema SPRM BNGS
+          Un proyecto del<br></br>ecosistema SPRM BNGS
         </p>
-        <p className="jimena">JIMENA GONZALEZ</p>
+        <div>
+          <a
+            href="mailto:contacto@labellezadelaoscuridad.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            contacto@labellezadelaoscuridad.com
+          </a>
+          <p className="jimena">JIMENA GONZALEZ</p>
+        </div>
         <p className="carlos">
-          arte por<br></br>Carlos Fama
+          Arte por<br></br>Carlos Fama
         </p>
       </div>
       <div className="creadores_mobile">
-        <p className="jimena">JIMENA GONZALEZ</p>
+        <div>
+          <p className="jimena">JIMENA GONZALEZ</p>
+          <a
+            href="mailto:contacto@labellezadelaoscuridad.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            contacto@labellezadelaoscuridad.com
+          </a>
+        </div>
         <div>
           <p className="supreme">
-            un proyecto <br></br>del ecosistema <br></br>SPRM BNGS
+            Un proyecto <br></br>del ecosistema <br></br>SPRM BNGS
           </p>
           <p className="carlos">
-            arte por<br></br>Carlos Fama
+            Arte por<br></br>Carlos Fama
           </p>
         </div>
       </div>
