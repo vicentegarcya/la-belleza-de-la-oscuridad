316bdf9f356db48ef39cf09c849ddcfa7af6c559
Vicente Garcia <vicentegarciaayllon@gmail.com>
2023-10-24 17:26:23 +0200

    add scroll changing title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.scss";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import card1 from "../src/images/1.jpg";
 import card2 from "../src/images/2.jpg";
 import card3 from "../src/images/3.jpg";
@@ -14,27 +14,41 @@ function App() {
   const titleRef = useRef();
   const creadoresRef = useRef();
   const cardsRef = useRef();
+  const fakeScrollRef = useRef();
+
+  const [text, setText] = useState(`La belleza de la oscuridad`);
 
   useEffect(() => {
-    setTimeout(() => proximamenteRef.current.classList.remove('hidden'), 500);
-    setTimeout(() => titleRef.current.classList.remove('hidden'), 1000);
+    fakeScrollRef.current.addEventListener("scroll", () => {
+      console.log(fakeScrollRef.current.scrollTop > 0);
+      if (fakeScrollRef.current.scrollTop > 0) {
+        setText("Contacto");
+      } else {
+        setText("La Belleza de la Oscuridad");
+      }
+    });
+
+    setTimeout(() => proximamenteRef.current.classList.remove("hidden"), 500);
+    setTimeout(() => titleRef.current.classList.remove("hidden"), 1000);
     setTimeout(() => {
-      explanationRef.current.querySelectorAll('p').forEach(e => {
-        e.classList.remove('hidden')
+      explanationRef.current.querySelectorAll("p").forEach((e) => {
+        e.classList.remove("hidden");
       });
-      creadoresRef.current.classList.remove('hidden');
+      creadoresRef.current.classList.remove("hidden");
     }, 2000);
-    setTimeout(() => cardsRef.current.classList.remove('hidden'), 2000);
+    setTimeout(() => cardsRef.current.classList.remove("hidden"), 2000);
     setTimeout(() => {
-      cardsRef.current.querySelectorAll('.card').forEach(e => {
-          e.classList.remove('hidden');
+      cardsRef.current.querySelectorAll(".card").forEach((e) => {
+        e.classList.remove("hidden");
       });
     }, 4000);
   }, []);
 
   return (
     <div className="App">
-      <div ref={proximamenteRef} className="proximamente hidden">próximamente</div>
+      <div ref={proximamenteRef} className="proximamente hidden">
+        próximamente
+      </div>
       <div ref={explanationRef} className="explanation">
         <p className="hidden">
           Un oráculo para quienes no creen en los oráculos, sino en conectar con
@@ -47,12 +61,32 @@ function App() {
       </div>
       <div className="title_cards">
         <div ref={cardsRef} className="cards hidden">
-          <img className="card hidden" src={card1} alt="first card of the deck"></img>
-          <img className="card hidden" src={card2} alt="second card of the deck"></img>
-          <img className="card hidden" src={card3} alt="third card of the deck"></img>
+          <img
+            className="card hidden"
+            src={card1}
+            alt="first card of the deck"
+          ></img>
+          <img
+            className="card hidden"
+            src={card2}
+            alt="second card of the deck"
+          ></img>
+          <img
+            className="card hidden"
+            src={card3}
+            alt="third card of the deck"
+          ></img>
           <img className="card" src={card4} alt="fourth card of the deck"></img>
-          <img className="card hidden" src={card5} alt="fifth card of the deck"></img>
-          <img className="card hidden" src={card6} alt="sixth card of the deck"></img>
+          <img
+            className="card hidden"
+            src={card5}
+            alt="fifth card of the deck"
+          ></img>
+          <img
+            className="card hidden"
+            src={card6}
+            alt="sixth card of the deck"
+          ></img>
           <img
             className="card hidden"
             src={card7}
@@ -60,20 +94,32 @@ function App() {
           ></img>
           <div className="cards_overlay"></div>
         </div>
-        <div ref={titleRef} className="title hidden">
-          La belleza<br></br> de la oscuridad
-        </div>
+        <a
+          ref={titleRef}
+          className="title hidden"
+          href="mailto:contacto@labellezadelaoscuridad.com"
+        >
+          {text.split(" ").slice(0, 2).join(" ")}
+          <br></br>
+          {text.split(" ").slice(2).join(" ")}
+        </a>
       </div>
-      <div ref={creadoresRef} className="creadores_desktop hidden">
-        <p className="supreme">
-          Un proyecto del<br></br>ecosistema SPRM BNGS
-        </p>
-        <div>
-          <p className="jimena">JIMENA GONZALEZ</p>
+      <div className="footer">
+        <div ref={creadoresRef} className="creadores_desktop hidden">
+          <p className="supreme">
+            Un proyecto del<br></br>ecosistema SPRM BNGS
+          </p>
+          <div>
+            <p className="jimena">JIMENA GONZALEZ</p>
+          </div>
+          <p className="carlos">
+            Arte por<br></br>Carlos Fama
+          </p>
         </div>
-        <p className="carlos">
-          Arte por<br></br>Carlos Fama
-        </p>
+        <p>* El arte de esta página es una muestra ilustrativa. Las ilustraciones de la baraja reflejarán una mayor diversidad étnica y racial, corporal y de género.</p>
+      </div>
+      <div ref={fakeScrollRef} className="fake_scroll">
+        <div></div>
       </div>
     </div>
   );
